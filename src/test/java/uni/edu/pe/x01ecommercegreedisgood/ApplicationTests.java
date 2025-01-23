05bb21e3576521bf9aedb127a75fb4099193f663
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-01-22 20:20:35 -0500

    new folders :)

---
+++ src/test/java/uni/edu/pe/x01ecommercegreedisgood/ApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.edu.pe.x01ecommercegreedisgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
